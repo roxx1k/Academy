1d85cd301dc7e4bd61528e0419fdcabf1d29d74a
j.hires <j.hires@quadient.com>
2017-10-09 15:43:40 +0200

    sad

---
+++ DataStructuresLibrary/Calendar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLibrary
+{
+    public class Calendar
+    {
+
+        public class CalendarEvent
+        {
+            public DateTime Time { get; set; }
+            public string Description { get; set; }
+            
+        }
+
+        private List<CalendarEvent> events { get; set; }
+        private int Eventcount { get; set; }
+        
+
+        public Calendar()
+        {
+            events = new List<CalendarEvent>();
+            Eventcount = 0;
+        }
+
+        public void CreateEvent(CalendarEvent myEvent)
+        {
+            
+        }
+
+        public void RemoveEvent(CalendarEvent myEvent)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+
+        }
+
+        public bool IsEmpty()
+        {
+            return Eventcount == 0;
+        }
+
+
+
+    }
+}
---
+++ DataStructuresLibrary/DatePriorityQueue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresLibrary
+{
+    public class DatePriorityQueue
+    {
+        public class Record
+        {
+            public DateTime Time { get; set; }
+            public String Message { get; set; }
+            public Record(DateTime time, String message)
+            {
+                Time = time;
+                Message = message;
+            }
+        }
+
+        private ParametricBothLinkedList<Record> records;
+
+        public DatePriorityQueue()
+        {
+            records = new ParametricBothLinkedList<Record>();
+        }
+
+        public void Insert(DateTime time, String msg)
+        {
+            ParametricBothLinkedList<Record>.Node first = records.AccessFirst();
+            if (first == null)
+            {
+                records.InsertFirst(new Record(time, msg));
+            }
+            else
+            {
+                ParametricBothLinkedList<Record>.Node temp = records.AccessAfter(first);
+                while(time < temp.Data.Time)
+                {
+                    temp = records.AccessAfter(temp);
+                }
+
+                if (first.Data.Time < temp.Data.Time)
+                {
+
+                }   
+            }
+            
+        }
+
+        public void PrintQueue()
+        {
+            if (records.IsEmpty())
+            {
+                Console.WriteLine("No Events planned");
+            }
+            else
+            {
+                ParametricBothLinkedList<Record>.Node first = records.AccessFirst();
+            }
+        }
+
+
+
+    }
+}
